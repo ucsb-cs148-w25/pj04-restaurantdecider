f0cdb258c4989b668e595216108aafe60345a903
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2025-01-17 06:10:46 -0800

    Winston's helloworld

---
+++ team/labs/winstonwang/HelloWorld/helloworld/helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by Winston Wang on 1/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

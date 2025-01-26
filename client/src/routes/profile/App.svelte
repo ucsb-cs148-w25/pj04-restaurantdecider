85fda6293f6b8d6f83fdcc90d1802e0ad0d296b4
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2025-01-26 14:52:31 -0800

    Add essential info for user profile

---
+++ client/src/routes/profile/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import UserProfile from './user_profile.svelte';
+
+    const user = {
+        email: 'user@example.com',
+        dietaryRestrictions: []
+    };
+</script>
+
+<div>
+    <UserProfile {user} />
+</div>
---
+++ client/src/routes/profile/user_profile.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let user;
+</script>
+
+<div>
+    <h1>User Profile</h1>
+    <p><strong>Email:</strong> {user.email}</p>
+    <p><strong>Dietary Restrictions:</strong> {user.dietaryRestrictions}</p>
+</div>

1238e26d22af5c6308f7b12f4acc14046de52cc8
the-irene-li <115101998+the-irene-li@users.noreply.github.com>
2025-01-29 14:21:14 -0800

    basic login and signup pages (#55)

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Weat!</h1>
+
+<p><a href="/login">Login</a></p>
+<p>Not have an account? Sign up <a href="/signup">here</a>!</p>
---
+++ client/src/routes/signup/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Sign Up</h1>
+
+<p>Username:</p>
+<input>
+
+<p>Password:</p>
+<input>
+
+<p>Reenter password:</p>
+<input>
+
+<p><a href="/..">Return Home</a></p>

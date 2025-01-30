c35fb6992ddf6ec8865f8940193c48ad52750ca7
Edison Zhang <edisonzhang@ucsb.edu>
2025-01-29 16:35:54 -0800

    please

---
+++ client/src/routes/signup/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+</script>
+
+<h1>Sign Up</h1>
+
+<p>Username/Email:</p>
+<Input placeholder="Username/Email" class="max-w-sm"></Input>
+
+<p>Password:</p>
+<Input placeholder="Password" class="max-w-sm"></Input>
+
+<p>Reenter password:</p>
+<Input placeholder="Password" class="max-w-sm"></Input>
+
+<p></p>
+<Button href="/..">Sign Up</Button>
+
+<p><a href="/..">Return Home</a></p>

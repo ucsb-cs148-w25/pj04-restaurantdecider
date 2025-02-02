c234876d46f5e59e4976696ff3403745f6ce6480
Tanay Biradar <34867487+TanayB11@users.noreply.github.com>
2025-02-02 11:35:58 -0800

    Move home page to / route

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen">
+  <Button
+    href="/login"
+    variant="ghost"
+    class="absolute right-12 top-8"
+  >
+    Sign In / Sign Up
+  </Button>
+
+  <div class="text-center space-y-4">
+    <h1 class="text-5xl font-bold">Weat</h1>
+    <p class="text-lg">Swipe until you find the perfect match!</p>
+
+    <Button
+      href="/restaurant_search"
+      variant="secondary"
+      class="text-white bg-black hover:bg-gray-600"
+    >
+      Start Searching
+    </Button>
+  </div>
+</div>

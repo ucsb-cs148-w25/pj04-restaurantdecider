c31e03efc5787902ee6b047688872b428f9206db
Tanay Biradar <34867487+TanayB11@users.noreply.github.com>
2025-02-03 15:01:58 -0800

    Add /api to the api base url (#86)

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -1,5 +1,5 @@
 export let apiBaseUrl = (
     (process.env.NODE_ENV == 'production')
     ?  'https://cs148.tanaybiradar.com/api'
-    : 'http://localhost:3000'
+    : 'http://localhost:3000/api'
 );

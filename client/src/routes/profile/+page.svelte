51f1afd65cdb7475a15dfea04078c4039287fea2
Chloe Weng <chloeweng@ucsb.edu>
2025-03-03 14:55:25 -0800

    profile card

--- client/src/routes/profile/+page.svelte
+++ client/src/routes/profile/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { Button } from '$lib/components/ui/button';
+  import { Button } from '$lib/components/ui/button';
 	import { getUsername } from '$lib/stores/userStore.svelte.js';
+  import * as Card from '$lib/components/ui/card';
 </script>
 
 <header class="fixed top-0 left-0 right-0 flex justify-between p-4 bg-white z-50">
@@ -13,6 +14,13 @@
 </header>
 
 <div class="pt-16 pb-24 min-h-screen">
-	<h1>Profile</h1>
-	<div>Username: {getUsername()}</div>
+	<div class="pl-4 text-4xl">Hi, {getUsername()}!</div>
+  <Card.Root class="mt-16">
+    <Card.Header class="text-center">
+      <Card.Title tag="h1" class="text-4xl">Past Rankings</Card.Title>
+    </Card.Header>
+    <Card.Content>
+      <p class="text-xl">temp</p>
+    </Card.Content>
+  </Card.Root>
 </div>

4e9062a743fdf4ba5fc8f958026fe8351b145eda
Irene Li <irene_li@ucsb.edu>
2025-03-06 00:38:34 -0800

    reformatted profile page and added sign out feature

--- client/src/routes/profile/+page.svelte
+++ client/src/routes/profile/+page.svelte
@@ -2,14 +2,31 @@
   import { Button } from '$lib/components/ui/button';
 	import { getUsername } from '$lib/stores/userStore.svelte.js';
   import * as Card from '$lib/components/ui/card';
+  import LogoNoMove from '$lib/images/WEAT_unmoving.png';
+  import { goto } from '$app/navigation';
+  import { getAuthToken } from '$lib/stores/userStore.svelte.js';
+	import { apiBaseUrl } from '$lib/index.js';
+  
+  async function handleSignOut() {
+		await fetch(`${apiBaseUrl}/users/signout`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${getAuthToken()}`
+			},
+			credentials: 'include'
+		});
+		goto('/');
+	}
 </script>
 
-<header class="fixed top-0 left-0 right-0 flex justify-between p-4 bg-white z-50">
-  <a href="/" class="text-lg font-bold text-black hover:underline">Weat</a>
+<header class="absolute top-0 left-0 right-0 flex justify-between p-4">
+  <a href="/"><img src={LogoNoMove} alt="Logo" style="width: 8rem"></a>
   <div class="space-x-2">
-    <form>
-      <Button type="submit" variant="outline" size="sm" class="bg-black text-white">Sign Out (Inoperative)</Button>
-    </form>
+    <form on:submit|preventDefault={handleSignOut}>
+			<Button type="submit" variant="outline" size="sm" class="bg-black text-white">Sign Out</Button
+			>
+		</form>
   </div>
 </header>
 

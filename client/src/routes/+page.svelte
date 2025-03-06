bf6e52e75594d6feacd8f3af4578b21ff28a34da
Irene Li <irene_li@ucsb.edu>
2025-03-05 22:33:32 -0800

    changed background color and fonts

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <header class="absolute top-0 left-0 right-0 flex justify-between p-4">
-  <a href="/"><img src={LogoNoMove} alt="Logo" style="width: 10rem"></a>
+  <a href="/"><img src={LogoNoMove} alt="Logo" style="width: 8rem"></a>
   <div class="space-x-2">
     <Button href="/login" variant="outline" size="sm" class="bg-black text-white">Sign In</Button>
     <Button href="/signup" variant="outline" size="sm" class="bg-black text-white">Sign Up</Button>
@@ -16,7 +16,6 @@
 <div class="flex flex-col items-center justify-center h-screen">
   <div class="text-center space-y-4">
     <img src={LogoMove} alt="Logo" style="width: 40rem">
-    
     <p class="text-lg">Decide on restaurants with your friends</p>
 
     <Button href="/login" variant="outline" size="sm" class="bg-black text-white">Sign In</Button>

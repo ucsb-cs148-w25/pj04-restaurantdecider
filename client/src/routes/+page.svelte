bbd3473fc75ea302582c243419fdf8bb87e5b078
Irene Li <irene_li@ucsb.edu>
2025-03-10 15:02:21 -0700

    changed the sign in page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -6,19 +6,16 @@
 
 <header class="absolute top-0 left-0 right-0 flex justify-between p-4">
   <a href="/"><img src={LogoNoMove} alt="Logo" style="width: 8rem"></a>
-  <div class="space-x-2">
-    <Button href="/login" variant="outline" size="sm" class="bg-black text-white">Sign In</Button>
-    <Button href="/signup" variant="outline" size="sm" class="bg-black text-white">Sign Up</Button>
-  </div>
 </header>
 
 
 <div class="flex flex-col items-center justify-center h-screen">
   <div class="text-center space-y-4">
-    <img src={LogoMove} alt="Logo" style="width: 40rem">
+    <img src={LogoMove} alt="Logo" style="width: 30rem">
     <p class="text-lg">Decide on restaurants with your friends</p>
 
-    <Button href="/login" variant="outline" size="sm" class="bg-black text-white">Sign In</Button>
-    <Button href="/signup" variant="outline" size="sm" class="bg-black text-white">Sign Up</Button>
+    <Button href="/login" variant="outline" size="lg" class="bg-black text-white py-5 px-8 text-lg mx-2">Sign In</Button>
+    
+    <Button href="/signup" variant="outline" size="lg" class="bg-black text-white py-5 px-8 text-lg mx-2">Sign Up</Button>
   </div>
 </div>
